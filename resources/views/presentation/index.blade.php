4c67f716cd2ba444c2370da50c817e0cc1f4214b
Francisco Alessandro da Silva Soares <alessandrosoares1409@gmail.com>
2025-05-11 22:51:43 -0300

    primeiras mudanças no header

--- resources/views/presentation/index.blade.php
+++ resources/views/presentation/index.blade.php
@@ -3,20 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}">
+
+    <!--Biblioteca de icones -->
+
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+     
     <title>Smart Telecon</title>
 </head>
 <body>
-    <header>
 
-        <img src="#" alt="emprise logo">
+    <!-- header e menu da aplicação com logo da empresa, menu de navegação, botão de tema e botão de contato rspectivamente-->
 
-        <nav>
+    <header class="header">
+
+        <img src="{{ asset('assets/img/project/LogoSmart.png') }}" alt="emprise logo" id="logo-image">
+
+        <nav class='menu-nav'>
+
+            <ul class="nav-menu-options">
+
+                <li class="menu-nav-option"><a href="#" class='link-menu'>Início</a></li>
+                <li class="menu-nav-option"><a href="#" class='link-menu'>Serviços</a></li>
+                <li class="menu-nav-option"><a href="#" class='link-menu'>Missão</a></li>
+                <li class="menu-nav-option"><a href="#" class='link-menu'>Valores</a></li>
+                <li class="menu-nav-option"><a href="#" class='link-menu'>Contatos</a></li>
+
+            </ul>
 
         </nav>
 
         <div class="theme-and-contact">
             
-            <button class="theme" name="theme">sol</button>
+            <button class="theme" name="theme"><i class="bi bi-sun"></i></button>
 
             <button class="contact-button" name="contact-button">conta-te nos</button>
 
