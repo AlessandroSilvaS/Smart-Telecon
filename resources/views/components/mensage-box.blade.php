b0a14042f6e14c5ac5c276fbc484f2e0ee9958fb
ideasgardendev <alessandrosoares1409@gmail.com>
2025-05-28 11:37:21 -0300

    mensage component

--- resources/views/components/mensage-box.blade.php
+++ resources/views/components/mensage-box.blade.php
@@ -1,3 +1,65 @@
-<div>
+<?php
+
+    $bgColor = null;
+
+    if($type && $type != null){
+
+        switch ($type) {
+            case 'sucess':
+                
+                $bgColor = '#28a745';
+
+                break;
+            
+            case 'error':
+
+                $bgColor = '#dc3545';
+
+                break;
+
+            case 'warning':
+
+                $bgColor = '##ffc107';
     
+                break;
+
+            default:
+
+                $bgColor = '#ffffff';
+
+                break;
+        }
+
+    }else{
+        echo 'error! \n type not defined';
+    }
+?>
+
+<div>
+
+    @if($title)
+
+        <div class="title-mensage">
+
+            {{$title}}
+    
+        </div>
+
+    @end
+
+    @if($text)
+
+        <div class="text-mensage">
+
+            {{$text}}
+
+        </div>
+
+    @end
+
+    @if($includeButton == true)
+
+        {{$includeButton}}
+
+    @endif
 </div>
